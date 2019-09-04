b38ca540ae476fca3fd6113c908543492d8378b0
LenaBuraja <lenusik343buraya@gmail.com>
2019-09-04 09:57:27 +0000

    Week 1. Unit 4

---
+++ webApp/Component.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+	[ 
+		"sap/ui/core/UIComponent"
+	],
+	function (UIComponent) { 
+		"use strict";
+		
+		return UIComponent.extend("SAP-1.webApp.Component", {
+			metadata: { manifest: "json" },
+			init : function () {
+				UIComponent.prototype.init.apply(this, arguments);
+			}
+		});
+	}
+);
