782b8aa599473e08699e6c5887202d01994e552e
LenaBuraja <lenusik343buraya@gmail.com>
2019-09-04 15:07:56 +0000

    Week 1. Unit 5/2

--- webApp/controllers/App.controller.js
+++ webApp/controllers/App.controller.js
@@ -7,7 +7,10 @@ sap.ui.define(
 		"use strict";
 		return Controller.extend("SAP-1.webApp.controllers.App", {
 			onShowHello : function () {
-				MessageBox.show("Hello openSAP", {
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+				MessageBox.show(sMsg, {
 				    icon: sap.m.MessageBox.Icon.NONE,
 				    title: "",
 				    actions: sap.m.MessageBox.Action.OK,
