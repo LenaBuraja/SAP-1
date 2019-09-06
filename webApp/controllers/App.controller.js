a811c8b753564dce9acb9155d9dd449faea29da0
LenaBuraja <lenusik343buraya@gmail.com>
2019-09-06 08:31:02 +0000

    Week 2. Unit 4/1

--- webApp/controllers/App.controller.js
+++ webApp/controllers/App.controller.js
@@ -2,9 +2,11 @@ sap.ui.define(
 	[
 		"sap/ui/core/mvc/Controller",
 		"sap/m/MessageBox",
-		"SAP-1/webApp/models/formatter"
+		"SAP-1/webApp/models/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator"
 	],
-	function (Controller, MessageBox, formatter) {
+	function (Controller, MessageBox, formatter,  Filter, FilterOperator) {
 		"use strict";
 		return Controller.extend("SAP-1.webApp.controllers.App", {
 			formatter : formatter,
@@ -14,13 +16,22 @@ sap.ui.define(
 				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 				MessageBox.show(sMsg, {
 				    icon: sap.m.MessageBox.Icon.NONE,
-				    title: "",
+				    title: "MessageBox",
 				    actions: sap.m.MessageBox.Action.OK,
 				    onClose: null,
 				    styleClass: "",
 				    initialFocus: null,
 				    textDirection: sap.ui.core.TextDirection.Inherit
 				});
+			},
+			onFilterProducts : function (oEvent) {
+				var aFilter = [], sQuery = oEvent.getParameter("query"),
+				oList = this.getView().byId("productsList"),
+				oBinding = oList.getBinding("items");
+				if (sQuery) {
+					aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+				}
+				oBinding.filter(aFilter);
 			}
 		});
 	}
